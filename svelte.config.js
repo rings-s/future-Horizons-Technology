c2921a7e7a32eaa52d9772be5975d61c4d0a1cf6
rings-s <rings9619@gmail.com>
2025-12-22 07:07:12 -0500

    fix the building error

--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '404.html'
-		}),
-		csrf: {
-			checkOrigin: true
-		}
+		})
 	}
 };
 
